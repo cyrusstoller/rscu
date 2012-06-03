cf07c03faa7d884db0d8d413241475c87e04fa4f
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-06-03 15:40:48 -0700

    Checking the full word list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,7 @@ class PagesController < ApplicationController
   def welcome
     @profiles = get_profiles
     
-    access_token = session[:access_token]
-    url = APP_CONFIG["singly_api_base"] + "/v0/types/statuses_feed"
-    @updates = HTTParty.get(url, :query => { :access_token => access_token, :q => "sad", :map => true }) if access_token
+    @updates = getUpdates(session[:access_token]) if session[:access_token]
   end
 
   def about
@@ -18,6 +16,20 @@ class PagesController < ApplicationController
     access_token = session[:access_token]
     url = APP_CONFIG["singly_api_base"] + "/profiles"
     HTTParty.get(url, :query => { :access_token => access_token }).parsed_response if access_token
+  end
+  
+  def getUpdates(access_token)
+    temp = []
+    url = APP_CONFIG["singly_api_base"] + "/v0/types/statuses_feed"
+    
+    APP_CONFIG["trigger_words"].each do |word|
+      temp += HTTParty.get(url, :query => { :access_token => access_token, :q => word, :dedup => true, :map => true }).parsed_response
+    end
+    
+    temp = temp.uniq do |t|
+      t["id"]
+    end
+    return temp
   end
   
 end
