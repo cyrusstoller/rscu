370333e27da3803838b7433b0dc34e3223659a36
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-06-03 12:58:13 -0700

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+require 'httparty'
+
+class PagesController < ApplicationController
+  def welcome
+    @profiles = get_profiles
+  end
+
+  def about
+  end
+  
+  def settings
+  end
+  
+  def pictures
+  end
+  
+  private
+  
+  def get_profiles
+    access_token = session[:access_token]
+    url = APP_CONFIG["singly_api_base"] + "/profiles"
+    HTTParty.get(url, :query => { :access_token => access_token }).parsed_response if access_token
+  end
+  
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+
+  describe "GET 'welcome'" do
+    it "returns http success" do
+      stub_request(:get, "https://api.singly.com/profiles?access_token=").
+               to_return(:status => 200, :body => "", :headers => {})
+               
+      get 'welcome'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'settings'" do
+    it "returns http success" do
+      get 'settings'
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'pictures'" do
+    it "returns http success" do
+      session[:access_token] = "foo"
+      get 'pictures'
+      response.should redirect_to root_path
+    end
+  end
+
+end
